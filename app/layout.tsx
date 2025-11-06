5d66f1db5d42fb84f9e732612be92fb1efbb171e
YavorStavrev <156719018+YavorStavrev@users.noreply.github.com>
2025-11-06 18:33:20 +0200

    Dark & Light Theme setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({
   // variable: "--font-geist-sans",
@@ -28,8 +29,11 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en" suppressHydrationWarning>
         <body
-          className={font.className}
-        ><ThemeProvider
+          className={cn(
+            font.className,
+            "bg-white dark:bg-[#313338]"
+          )}>
+            <ThemeProvider
           attribute="class" 
           defaultTheme="dark"
           enableSystem={false}
